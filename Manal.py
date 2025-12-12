0a1ce45dbc252c63e83ae0517f6ac8ef5c8901ba
oussamablb22-star <oussama.blb22@gmail.com>
2025-12-12 16:25:36 +0100

    Update Manal.py

--- Manal.py
+++ Manal.py
@@ -5,5 +5,38 @@
 #liste des membres :
 #imade belabbes
 #chabanesari dounia zed
+1_création du tableau de séquences ADN = [
+    "ATGCGTACGTA",
+    "GCTAGCTAGGCC",
+    "ATGCGCGTAAGT",
+    "TACGATCGTA",
+    "ATGAAGGCTT",
+    "CGTACGTAGC",
+    "TTAACCGGAT"]df = pd.DataFrame({
+    "Séquence": sequences
+})
+# Calcul automatique de la longueur
+df["Longueur"] = df["Séquence"].apply(len)
+
+# Fonction pour calculer le pourcentage de GC
+def pourcentage_gc(seq):
+    seq = seq.upper()
+    gc = seq.count("G") + seq.count("C")
+    return (gc / len(seq)) * 100
+
+# Ajout du pourcentage GC
+df["Pourcentage_GC"] = df["Séquence"].apply(pourcentage_gc).round(2)
+
+# 2) Sélection et affichage de la colonne Longueur
+print("=== Colonne Longueur ===")
+print(df[["Longueur"]])
+print()
+
+# 3) Filtrer les séquences dont la longueur est supérieure à 10
+df_filtre = df[df["Longueur"] > 10]
+print("=== Séquences avec longueur > 10 ===")
+print(df_filtre)
+print()
+
 
 
