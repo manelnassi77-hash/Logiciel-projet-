31dd5ab9af99ec091d8552fbf9a50ad8c01dd01c
Manal nassi <manelnassi77@gmail.com>
2025-12-12 16:12:01 +0100

    Update Manal.py

--- Manal.py
+++ Manal.py
@@ -1,1 +1,9 @@
+#Master 1 : microbiologie fondamentale
+#Chef de groupe : Nassi Manal
+#lien de reposetory : https://github.com/manelnassi77-hash/Logiciel-projet-/blob/main/Manal.py
+#la date : 11/12/2025
+#liste des membres :
+#imade belabbes
+#chabanesari dounia zed
+
 
